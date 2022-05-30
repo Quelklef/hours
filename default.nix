4085779fd72760619e2c52d1b2fb191e5a01b76b
Mason Mackaman <masondeanm@aol.com>
2022-05-30 16:52:29 -0400

    Pin nixpkgs
    Due to icu updating, hours broke

--- default.nix
+++ default.nix
@@ -1,4 +1,11 @@
-{ pkgs ? import <nixpkgs> {}
+{ pkgs
+  ? import
+      (builtins.fetchTarball
+         { url = "https://github.com/NixOS/nixpkgs/archive/33772708c6d0e33f697426ba386aa0149cbcbecb.tar.gz";
+           sha256 = "0l9s56vbh9ljndc1g3fm2byzfwdc0d1gjh21lqyvnjqzqwbc0gfm";
+         }
+      )
+      { inherit system; }
 , system ? builtins.currentSystem
 }:
 ( import ./nix.nix { inherit pkgs system; } ).deriv
