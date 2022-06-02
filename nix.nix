b991e8c0f27a6743c3d922364b889383bb2a768e
Maynard <elimaynard923@gmail.com>
2022-06-02 15:40:11 -0400

    make nixpkgs pin

--- nix.nix
+++ nix.nix
@@ -1,4 +1,13 @@
+let f = out-attr:
+
 { pkgs
+  ? import
+      (builtins.fetchTarball
+         { url = "https://github.com/NixOS/nixpkgs/archive/33772708c6d0e33f697426ba386aa0149cbcbecb.tar.gz";
+           sha256 = "0l9s56vbh9ljndc1g3fm2byzfwdc0d1gjh21lqyvnjqzqwbc0gfm";
+         }
+      )
+      { inherit system; }
 , system ? builtins.currentSystem
 }:
 
@@ -82,4 +91,6 @@ in {
     '';
   };
 
-}
+}.${out-attr};
+
+in { deriv = f "deriv"; shell = f "shell"; }
--- shell.nix
+++ shell.nix
@@ -1,4 +1,1 @@
-{ pkgs ? import <nixpkgs> {}
-, system ? builtins.currentSystem
-}:
-( import ./nix.nix { inherit pkgs system; } ).shell
+(import ./nix.nix).shell
